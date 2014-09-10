b8581a502ad1ca95ebfa285106813d8a56f677f7
Nikhil Mathew <nikhil@zenpayroll.com>
2014-09-09 23:36:07 -0700

    CA setup

--- app/states/ca.rb
+++ app/states/ca.rb
@@ -1,9 +1,12 @@
 StateBuilder.build('CA') do
   company do
-    # CompanyStateField::CA configuration
+    edd { format '\d{3}-\d{4}-\d' }
+    sos { format '[A-Z]\d{7}' }
   end
   
   employee do
-    # EmployeeStateField::CA configuration
+    filing_status { options ['Single', 'Married', 'Head of Household'] }
+    withholding_allowance { max 99 }
+    additional_withholding { max 10000 }
   end
 end
