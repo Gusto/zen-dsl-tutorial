75d695eb607c0adbeaa6cad709871c0e2ca19636
Nikhil Mathew <nikhil@zenpayroll.com>
2014-09-09 23:35:02 -0700

    State shell and state builder shell

--- app/models/state_builder.rb
+++ app/models/state_builder.rb
@@ -1,6 +1,7 @@
 class StateBuilder
   def self.build(state, &block)
-    raise unless block_given?
+    # If no block is passed in, raise an exception
+    raise "You need a block to build!" unless block_given?
 
     StateBuilder.new(state, &block)
   end
@@ -8,14 +9,15 @@ class StateBuilder
   def initialize(state, &block)
     @state = state
 
+    # Evaluate the contents of the block passed in within the context of this instance of StateBuilder
     instance_eval &block
   end
 
   def company(&block)
-    StateBuilder::CompanyScope.new(@state, &block)
+    # Configure CompanyStateField::CA
   end
 
   def employee(&block)
-    StateBuilder::EmployeeScope.new(@state, &block)
+    # Configure EmployeeStateField::CA
   end
 end
--- app/states/ca.rb
+++ app/states/ca.rb
@@ -1,15 +1,9 @@
 StateBuilder.build('CA') do
   company do
-    edd { format '/\d{3}-\d{4}-\d/' }
-    sos { format '/\d{4}/' }
+    # CompanyStateField::CA configuration
   end
   
   employee do
-    withholding_allowance { max 20 }
-    additional_withholding { max 4 }
-
-    filing_status do
-      options ['Single', 'Married', 'Married withhold as Single', 'Exempt']
-    end
+    # EmployeeStateField::CA configuration
   end
 end
